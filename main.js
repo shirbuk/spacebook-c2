5c2e19fb597a1c0e8b442c592d46fd03d2858a00
New Computer <admin@Sarahs-MacBook-Air.local>
2017-12-28 13:14:26 +0200

    changes

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 var posts = [];
 var idUnique = 1;
 var text = $('#post-name').val()
+var comments = [];
 
 //add post to the posts:
 function addPosts(text, id) {
     var post = {
         "text": text,
-        "id": id
+        "idUniqe": id,
     }
     posts.push(post);
 };
+
 
 $('.btn').on('click', function () {
     addPosts($('#post-name').val(), idUnique);
@@ -20,12 +22,23 @@ $('.btn').on('click', function () {
 function renderPost() {
     $('.posts').empty();
     for (var i = 0; i < posts.length; i++) {
-        $('.posts').append('<div data-id="' + posts[i].id + '">' + posts[i].id + ' ' + posts[i].text + '<button type="button" class="remove">REMOVE</button></div>');
+        $('.posts').append
+        ('<div data-id="' + posts[i].id + '">' 
+        + posts[i].id + ' ' + posts[i].text 
+        + '<button type="button" class="remove">REMOVE</button>' 
+        + '<input class="add-Comment" type="text" placeholder="leave a comment">'
+        + '<button type="button" class="comment-Button">COMMENT</button></div>');
     }
 }
 
 $('.posts').on('click','.remove', function () {
-    $('this'). 
+    var removePosts = $('this').closest("div").data().id;
+    posts.splice(removePosts, 1);
+    renderPost();
 })
 
 
+$('.btn').on('click','.add-comment', function() {
+    var addIt = $('this').closest("div").data().id;
+    renderPost();
+})
